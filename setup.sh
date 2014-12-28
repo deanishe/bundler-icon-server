bc4745c54fe9de5840e70866e478205a376ba1e9
Dean Jackson <deanishe@deanishe.net>
2014-12-29 00:52:26 +0100

    Make `setup.sh` work with bash
    Fix #1

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
-#!/bin/zsh
+#!/bin/bash
 
 here=$(dirname "$0")
 curdir=$(pwd)
-chdir "$here"
+cd "$here"
+
+# Create virtualenv and install server dependencies into it
 [[ ! -d "env" ]] && {
 	echo "Creating virtualenv ..."
 	virtualenv env
@@ -10,18 +12,19 @@ chdir "$here"
 echo "Installing required packages ..."
 env/bin/pip install -r requirements.txt
 
+# Create directories needed for the server to run
 echo "Creating directories ..."
 
 [[ ! -d "log" ]] && mkdir log
 [[ ! -d "iconserver/static/icons" ]] && mkdir "iconserver/static/icons"
 
-
+# Create sample configuration file
 [[ ! -f siteconfig.py ]] && {
 	echo "Creating siteconfig.py"
 	cp siteconfig.sample.py siteconfig.py
 }
 
-chdir "$curdir"
+cd -
 
 echo 'Run `run.py` to start the test server at localhost:5000'
 echo 'Be sure to edit `siteconfig.py` first, otherwise the server will not run!'
